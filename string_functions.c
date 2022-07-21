93d627d697c5902cd6fe27878533bd46948930f4
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-07-21 19:10:11 +0530

    strlen,strcopy,strcat

---
+++ string_functions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char str[25]="GG" ,str1[4];
+	printf("%d\n",(int) strlen(str));
+	strcpy(str1,str);
+	printf("%s\n",str1);
+	printf("%s\n",str);
+	strcat(str1,str);
+	printf("%s\n",str1);
+
+	return 0;
+}
