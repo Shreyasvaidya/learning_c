dbdf6f7374f93d5e440d050c612ebeab451468be
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-07-22 13:19:18 +0530

    increase length to 5
    i don't know how it compiled

--- string_functions.c
+++ string_functions.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char str[25]="GG" ,str1[4];
+	char str[25]="GG" ,str1[5];
 	printf("%d\n",(int) strlen(str));
 	strcpy(str1,str);
 	printf("%s\n",str1);
